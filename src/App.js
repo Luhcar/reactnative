38f298f3d4879ec8ec3142dd3c17079c05bbb94d
Luhcar <rachulindra24@gmail.com>
2024-05-04 10:40:07 +0700

    nambah-Use-Effect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Counter from './pages/Counter'
+import SplashScreen from './pages/SplashScreen'
+import { useState } from 'react'
 
 const App = () => {
+  const [isSplash, setSplash] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setSplash(false)
+    }, 2000)
+  })
+
   return (
     <>
       {/* <Counter /> */}
+
       {/* <Home /> */}
-      <Login />
+
+      {/* <Login /> */}
+      
+      {isSplash ? <SplashScreen/> :  <Login/>}
     </>
   )
 }
